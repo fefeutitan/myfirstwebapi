580cc6b91bfcb30a594e28aa17b8e42bd6041216
Fernando Ellery <fernando.ellery@camara.leg.br>
2022-09-18 16:20:48 -0300

    inclusão de usuarios no banco h2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_usuario(nome, email, password) VALUES('Fefeu', 'fefeu@email.com', '123456');
+INSERT INTO tb_usuario(nome, email, password) VALUES('Lulu', 'lulu@email.com', '123456');
